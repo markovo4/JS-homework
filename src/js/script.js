7c472531b2ce17cf6c2583cda62f013f4aec7dc6
markovo4 <obf2007@gmail.com>
2023-12-14 14:47:57 +0200

    Homework-2

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+let a = prompt("Enter the first variable value: ",0);
+let b= prompt("Enter the second variable value: ", 0);
+
+let sum = +a + +b;
+alert( a + "+" + b + '=' + sum)
+
+let subtraction = +a - +b;
+alert( a + "-" + b + '=' + subtraction)
+
+let division = +a / +b;
+alert( a + "/" + b + '=' + division)
+
+let multiplication = +a * +b;
+alert( a + "*" + b + '=' + multiplication)
