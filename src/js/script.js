78ee64ea0779d2501beb7768ef28c7550a1c5b09
markovo4 <obf2007@gmail.com>
2023-12-14 14:27:43 +0200

    Homework-2

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+// Task #1
+let admin, name;
+
+name = "John";
+admin = name;
+
+alert(admin)
+
+
+// Task#2
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+
+// Task #3
+let twoNumbers = "10" + 10
+alert(typeof(twoNumbers))
+alert(twoNumbers)
