f15f4e8fb8ca17f573535027fd8201c55c395e66
markovo4 <obf2007@gmail.com>
2023-12-28 23:14:06 +0200

    Homework-9

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const firstNum = prompt("Please enter first number", );
+const secondNum = prompt("Please enter first number", );
+const thirdNum = prompt("Please enter first number", );
+
+alert(`(${firstNum} + ${secondNum} + ${thirdNum}) / 3 = ${(+firstNum + +secondNum + +thirdNum)/3}`)
+
