e06d47fd48047ad1c2cfac607ee97dd7bd1f3bde
Volodymyr Kirillov <143116613+markovo4@users.noreply.github.com>
2024-01-08 16:44:20 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,11 @@
 
 const num = +prompt("Please enter your number: ", '');
 let smallestDiv = 0;
-Outer:
+
 for(let i = 2; i <= num; i++){
     if(num % i === 0){
         smallestDiv = i;
-        break Outer;
+        break;
     }
 }
 console.log(`Smallest divisor: ${smallestDiv}`)
