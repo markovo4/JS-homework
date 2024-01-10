143df0fde611df4a2b1d901ba97278863464e6e0
markovo4 <obf2007@gmail.com>
2024-01-11 00:16:37 +0200

    Homework-19

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function generateKey(length, characters){
+    let pick = '';
+    while(pick.length < length) pick += characters[Math.floor(Math.random()*characters.length)];
+    return pick;
+}
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+
+console.log(key)
+
+
+
+
+
+
