4c919051bd467a72d8f1f121afadbeff524d376b
markovo4 <obf2007@gmail.com>
2024-01-11 01:09:50 +0200

    Homework-20

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function bringToPowerOf(number, power = 1){
+    if(isNaN.number || isNaN.power) return console.log("Error");
+    return number**power;
+}
+const number = +prompt("Please enter your number", '');
+const power = +prompt("Please enter your number", '');
+
+const result = bringToPowerOf(number, power);
+alert(result);
