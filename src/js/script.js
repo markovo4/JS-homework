81985d8da7eee4310cc4f7ee88bec3f26b4bb64c
markovo4 <obf2007@gmail.com>
2024-01-11 14:45:51 +0200

    Homework-23

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function getPotato(volume = 1){
+
+    if(isNaN(volume) || volume === 0) return "Error";
+    const potatoWeight = Math.ceil(((volume*4*75)/1000));
+    const potatoPrice = potatoWeight*13;
+    return `For ${volume} liter(s) of Soup, you will need ${potatoWeight} kg of potato which will cost you ${potatoPrice} UAH`;
+
+}
+
+const volume = +prompt("Please enter the volume of soup, ingredients for you would like to calculate: ")
+alert(getPotato(volume))
+console.log(getPotato(volume))
