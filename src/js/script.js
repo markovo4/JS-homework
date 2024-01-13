6c0ac0dc7457b0fa07f535e4eface897afe181a0
markovo4 <obf2007@gmail.com>
2024-01-13 20:10:03 +0200

    Homework-24

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,17 @@
 'use strict';
 
 function defWordEnding(userAge){
-    switch(true){
-        case userAge === null || userAge === 0:
-            return 'Error';
-        case userAge % 10 === 1 && userAge % 100 !== 11:
-            return `Вам ${userAge} год!`;
-        case userAge % 10 >= 2 && userAge % 10 <= 4 && userAge % 100 !== 12  && userAge % 100 !== 13 && userAge % 100 !== 14:
-            return `Вам ${userAge} года!`;
-        default:
-            return `Вам ${userAge} лет!`;
+
+    if(userAge === null || userAge === 0){
+        return 'Error';
+    } else if(userAge % 10 === 1 && userAge % 100 !== 11){
+        return `Вам ${userAge} год!`;
+    } else if(userAge % 10 >= 2 && userAge % 10 <= 4 && userAge % 100 !== 12  && userAge % 100 !== 13 && userAge % 100 !== 14) {
+        return `Вам ${userAge} года!`;
+    } else {
+        return `Вам ${userAge} лет!`;
     }
+
 }
 const userAge = +prompt("Please enter your age: ", );
 alert(defWordEnding(userAge))
