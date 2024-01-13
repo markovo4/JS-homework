fb473c4d375fcc2623ab4a4c129d50b3204ec9a6
markovo4 <obf2007@gmail.com>
2024-01-13 20:23:05 +0200

    Homework-24

--- src/js/script.js
+++ src/js/script.js
@@ -5,14 +5,14 @@ function defWordEnding(userAge){
     if(userAge === null || userAge === 0){
         return 'Error';
     } else if(userAge % 10 === 1 && userAge % 100 !== 11){
-        return `Вам ${userAge} год!`;
+        return `год`;
     } else if(userAge % 10 >= 2 && userAge % 10 <= 4 && userAge % 100 !== 12  && userAge % 100 !== 13 && userAge % 100 !== 14) {
-        return `Вам ${userAge} года!`;
+        return `года`;
     } else {
-        return `Вам ${userAge} лет!`;
+        return `лет`;
     }
 
 }
 const userAge = +prompt("Please enter your age: ", );
-alert(defWordEnding(userAge))
+alert(`Вам ${userAge} ${defWordEnding(userAge)}!`)
 
