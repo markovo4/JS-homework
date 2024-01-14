a0b52a6355b03bdb24bdfa245ba5859226486e53
markovo4 <obf2007@gmail.com>
2024-01-14 11:58:14 +0200

    Homework-23

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 
 function getPotato(volume = 1){
 
-    if(isNaN(volume) || volume === 0) return "Error";
+    if(isNaN(volume) || volume === 0 || typeof(Number)) return "Error";
     const potatoWeight = Math.ceil(((volume*4*75)/1000));
     const potatoPrice = potatoWeight*13;
     return `For ${volume} liter(s) of Soup, you will need ${potatoWeight} kg of potato which will cost you ${potatoPrice} UAH`;
