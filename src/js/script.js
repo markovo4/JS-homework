83e437854a00eac711ba52923cd4e71d99cfe036
markovo4 <obf2007@gmail.com>
2024-01-15 15:40:40 +0200

    Homework-25

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,12 @@
 'use strict';
 
-//Please pick the one that is easier to read
-
-
-//Version 1
 const revRandNum = function(){
 
     //Generates a random number
-    let randomNum = Math.floor(Math.random()*101);
+    const randomNum = Math.floor(Math.random()*101);
 
     //Converts a random number into a string, splits the string, then reverses it, joins it back together, and converting it back into a number.
-    let reverseNum = parseInt(randomNum
+    const reverseNum = parseInt(randomNum
             .toString()
             .split('')
             .reverse().join('')
@@ -20,28 +16,6 @@ const revRandNum = function(){
 
 for (let i = 0; i < 100; i++){
     console.log(revRandNum());
-}
-
-
-
-
-//Version 2
-const revRandNum2 = function() {
-
-    //Generates a random number, converts it into a string, splits the string, then reverses it, joins it back together, and converting it back into a number.
-    let reverseNum2 = parseInt(Math.floor(Math.random() * 101)
-            .toString()
-            .split('')
-            .reverse().join('')
-        , 10);
-    return reverseNum2;
-
-}
-
-let i = 0;
-while(i < 100){
-    console.log(revRandNum2());
-    i++;
 }
 
 
