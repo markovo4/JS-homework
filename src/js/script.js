f7ffff819f309f8a5ce2e2804a94368f8a150da0
markovo4 <obf2007@gmail.com>
2024-01-15 22:43:53 +0200

    Homework-25

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,73 @@
 'use strict';
 
 const revRandNum = function(){
-
-    const randomNum = Math.floor(Math.random()*150);
-    const reverseNum = parseInt(randomNum
-            .toString()
-            .split('')
-            .reverse()
-            .join('')
-        , 10);
-    return reverseNum;
+    return Math.floor(Math.random()*101);
 }
 
 const arrayOfUniqueNum = [];
-
 
 while(arrayOfUniqueNum.length !== 100){
 
     let num = revRandNum();
     if (!arrayOfUniqueNum.includes(num)) {
         arrayOfUniqueNum.push(num);
+        console.log(num);
     }
 }
 
-console.log(arrayOfUniqueNum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 'use strict';
+//
+// const revRandNum = function(){
+//
+//     const randomNum = Math.floor(Math.random()*101);
+//     const reverseNum = parseInt(randomNum
+//             .toString()
+//             .split('')
+//             .reverse()
+//             .join('')
+//         , 10);
+//     return reverseNum;
+// }
+//
+// const arrayOfUniqueNum = [];
+//
+//
+// while(arrayOfUniqueNum.length !== 100){
+//     let num = revRandNum();
+//     if (!arrayOfUniqueNum.includes(num)) {
+//         arrayOfUniqueNum.push(num);
+//     }
+// }
+//
+// console.log(arrayOfUniqueNum);
 
 
 
