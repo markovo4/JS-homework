694983634ac26a49e0a8746a4f5141775a58b493
markovo4 <obf2007@gmail.com>
2024-01-15 17:36:56 +0200

    Homework-25

--- src/js/script.js
+++ src/js/script.js
@@ -2,21 +2,28 @@
 
 const revRandNum = function(){
 
-    //Generates a random number
     const randomNum = Math.floor(Math.random()*101);
-
-    //Converts a random number into a string, splits the string, then reverses it, joins it back together, and converting it back into a number.
     const reverseNum = parseInt(randomNum
             .toString()
             .split('')
             .reverse().join('')
         , 10);
     return reverseNum;
+
 }
 
-for (let i = 0; i < 100; i++){
-    console.log(revRandNum());
+const arrayOfUniqueNum = [];
+
+while(arrayOfUniqueNum.length !== 100){
+    let num = revRandNum();
+    if(arrayOfUniqueNum.find(num) !== num){
+        arrayOfUniqueNum.push(num);
+    }
 }
+console.log(arrayOfUniqueNum);
+
+
+
 
 
 
