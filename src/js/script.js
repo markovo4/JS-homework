a187122ad5978c58a1d15241df7c32932e51e89a
markovo4 <obf2007@gmail.com>
2024-01-16 16:59:41 +0200

    Homework-25

--- src/js/script.js
+++ src/js/script.js
@@ -4,13 +4,13 @@ const arrayOfUniqueNum = [];
 
 const revRandNum = function() {
 
-    let randNum = Math.floor(Math.random() * 100);
+    let randNum = Math.floor(Math.random() * 101);
 
     if (!arrayOfUniqueNum.includes(randNum)) {
         arrayOfUniqueNum.push(randNum);
         return randNum;
     } else while (arrayOfUniqueNum.includes(randNum)) {
-        randNum = Math.floor(Math.random() * 100 + 1);
+        randNum = Math.floor(Math.random() * 101);
     }
 
     arrayOfUniqueNum.push(randNum);
