a9908432e8b10ad0d48c2fd20eda7097606999af
markovo4 <obf2007@gmail.com>
2024-01-27 16:31:13 +0200

    Homework-26

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,34 @@
-const greetings = function () {
-  return `Hello, ${this.name}`;
-};
-
-const showAge = function () {
-  return `${this.name}, you are ${this.age} years old!`;
-};
-
 const createUser = function (name, age) {
+  const greetings = function () {
+    return `Hello ${this.name}!`;
+  };
+
+  const showAge = function () {
+    return `${this.name}, you are ${this.age} years old!`;
+  };
+
   const user = {
     name,
     age,
+    greetings,
+    showAge,
   };
-
-  user.greetings = greetings;
-
-  user.showAge = showAge;
 
   return user;
 };
 
 const newUser1 = createUser('Vlad', 20);
+
 console.log(newUser1.greetings());
 console.log(newUser1.showAge());
 
 const newUser2 = createUser('Vova', 20);
+
 console.log(newUser2.greetings());
 console.log(newUser2.showAge());
+
+if (newUser2.showAge() === newUser1.showAge()) {
+  console.log('True');
+} else {
+  console.log('False');
+}
