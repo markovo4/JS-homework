24e9dae5fe92297f51d26f259919951f0244b8a6
markovo4 <obf2007@gmail.com>
2024-02-13 19:26:12 +0200

    Homework-34

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+// Task 1
+const getFactorial = function (number) {
+  let factorial = 0;
+  if (number === 0) {
+    factorial = 1;
+  } else {
+    factorial = number * getFactorial(number - 1);
+  }
+  return factorial;
+};
+
+console.log(getFactorial(10));
+
+// Task 2
+const pow = function (num, degree) {
+  let result;
+  if (degree === 0) {
+    result = 1;
+  } else {
+    result = num * pow(num, degree - 1);
+  }
+  return result;
+};
+
+console.log(pow(2, 5));
+
+// Task 3
+const customSum = function (num, counter) {
+  let result;
+  if (counter === 0) {
+    result = 1;
+  } else {
+    result = num + customSum(1, counter - 1);
+  }
+  return result;
+};
+
+console.log(customSum(15, 20));
