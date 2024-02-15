2155bc7808e9b149aa6b2b4af477ed2f8baeb0b4
markovo4 <obf2007@gmail.com>
2024-02-15 17:47:19 +0200

    Homework-36

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+const ladder = {
+  step: 0,
+  up() {
+    this.step += 1;
+    return this;
+  },
+  down() {
+    this.step -= 1;
+    return this;
+  },
+  showStep() {
+    return this.step;
+  },
+};
+
+console.log(ladder.up().up().down().showStep());
